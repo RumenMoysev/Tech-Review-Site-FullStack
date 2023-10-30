29b89e89a491c05145b1e4f67dd1da77aa8fd63d
RumenMoysev <rumoysev@gmail.com>
2023-10-30 20:33:48 +0200

    Themes are now working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,28 @@
+import { useEffect } from "react"
+
 export default function Header() {
+    
+    useEffect(() => {
+        document.querySelector('.themes button').addEventListener('click', onclickShowThemes)
+    })
+
+    useEffect(() => {
+        document.querySelector(".homeNavIcon").addEventListener("click", onClickHomePageRedirect);
+    })
+
+    useEffect(() => {
+        let themeContainer = document.querySelector(".colorsContainer");
+        themeContainer.style.setProperty("--theme-color", defaultTheme);
+
+        let themes = document.querySelectorAll(".colorsContainer li img");
+
+        for (let el of themes) {
+            el.addEventListener("mouseenter", hoverOnTheme);
+            el.addEventListener("mouseleave", unHoverTheme);
+            el.addEventListener("click", onclickChangeTheme);
+        }
+    })
+    
     return (
         <header id="header">
             <img
@@ -11,7 +35,7 @@ export default function Header() {
                 <ul className="nav_links">
                     <div id="userNavLinks">
                         <li><a href="/reviews">Reviews</a></li>
-                        <li className="welcome"><p>Welcome, guest</p></li>
+                        <li className="welcome"><p >Welcome, guest</p></li>
                         <li><a href="javascript:void(0)">Logout</a></li>
                     </div>
                     <div id="guestNavLinks">
@@ -58,4 +82,80 @@ export default function Header() {
             </div>
         </header>
     )
+}
+
+let themesShown = false;
+
+function onclickShowThemes(e) {
+    if (!themesShown) {
+        e.target.parentElement.children[1].style.display = "flex";
+        themesShown = true;
+    } else {
+        e.target.parentElement.children[1].style.display = "none";
+        themesShown = false;
+    }
+}
+
+function onClickHomePageRedirect() {
+    window.location.href = "/";
+}
+
+//////////////////////////////////////////////////
+
+const themeLogos = {
+    red: "images/logoTred.png",
+    green: "images/logoTgreen.png",
+    blue: "images/logoTblue.png",
+    yellow: "images/logoTyellow.png",
+};
+
+let defaultTheme = "#1e9bff";
+
+const themeButtonColors = {
+    red: ["rgba(175, 0, 0, 1)", "rgba(175, 0, 0, 0.7)", "rgba(175, 0, 0, 0.4)"],
+    green: [
+        "rgba(11, 116, 1, 1)",
+        "rgba(11, 116, 1, 0.7)",
+        "rgba(11, 116, 1, 0.4)",
+    ],
+    blue: [
+        "rgba(0, 136, 169, 1)",
+        "rgba(0, 136, 169, 0.7)",
+        "rgba(0, 136, 169, 0.4)",
+    ],
+    yellow: [
+        "rgba(199, 186, 10, 1)",
+        "rgba(199, 186, 10, 0.7)",
+        "rgba(199, 186, 10, 0.4)",
+    ],
+};
+
+const themeColors = {
+    red: "red",
+    green: "green",
+    blue: "#1e9bff",
+    yellow: "yellow",
+};
+
+// const body = document.querySelector('body')
+
+function hoverOnTheme(e) {
+    let changedShadow = document.querySelector(".colorsContainer");
+    changedShadow.style.setProperty("--theme-color", themeColors[e.target.id]);
+}
+
+function unHoverTheme() {
+    let changedShadow = document.querySelector(".colorsContainer");
+    changedShadow.style.setProperty("--theme-color", defaultTheme);
+}
+
+function onclickChangeTheme(e) {
+    let id = e.target.id;
+    let theme = themeColors[id];
+    defaultTheme = theme;
+    body.style.setProperty("--theme-color", defaultTheme);
+    body.style.setProperty("--button-color", themeButtonColors[id][0]);
+    body.style.setProperty("--button-hover-color", themeButtonColors[id][1]);
+    body.style.setProperty("--button-active-color", themeButtonColors[id][2]);
+    homeNavIcon.src = themeLogos[id];
 }
