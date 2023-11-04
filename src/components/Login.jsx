cf9feec3298f172522fd30fce3fec3a76dd63aec
RumenMoysev <rumoysev@gmail.com>
2023-11-04 14:51:24 +0200

    Add login handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,44 @@
+import { useState } from "react";
+
 export default function Login() {
+
+    const [error, setError] = useState(undefined)
+
+    async function loginHandler(e) {
+        e.preventDefault()
+
+        const formData = new FormData(e.target.parentElement)
+
+        const userData = {
+            email: formData.get('email'),
+            password: formData.get('password')
+        }
+
+        const settings = {
+            method: "POST",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(userData)
+        }
+
+        const response = await fetch('http://localhost:3030/users/login', settings)
+        const json = await response.json()
+
+        if(!response.ok) {
+            return setError(json.message)
+        }
+
+        sessionStorage.setItem('auth', json.authToken)
+        sessionStorage.setItem('email', json.email)
+        sessionStorage.setItem('username', json.username)
+        sessionStorage.setItem('userId', json.userId)
+    }
+
     return (
         <section id="loginPage" className="hidden">
             <div className="form-container">
                 <div className="formHeaders">
                     <h2>Login</h2>
-                    <h4>Invalid email!</h4>
+                    {error && <h4>{error}</h4>}
                 </div>
                 <form id="loginForm">
                     <label htmlFor="emailInput">Email</label>
@@ -33,7 +67,7 @@ export default function Login() {
                         </label>
                         <input type="checkbox" id="logRegCheckbox" />
                     </div>
-                    <button type="submit" id="buttons">
+                    <button type="submit" id="buttons" onClick={loginHandler}>
                         Login
                     </button>
                 </form>
