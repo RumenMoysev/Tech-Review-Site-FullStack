1c3a71c0f9ebde0eff18743a7df996541acd5100
RumenMoysev <rumoysev@gmail.com>
2023-11-06 17:54:25 +0200

    Add reviewsController to routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const userController = require('./controllers/userController.js')
+const reviewsController = require('./controllers/reviewsController.js')
 
 router.use('/users', userController)
+router.use('/data/reviews', reviewsController)
 
 module.exports = router
