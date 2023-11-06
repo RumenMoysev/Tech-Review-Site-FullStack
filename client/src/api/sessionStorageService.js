a8e3ab547bc90e2f0a4f936e28c94196bbbf153d
RumenMoysev <rumoysev@gmail.com>
2023-11-06 17:55:16 +0200

    Moved session storage logic to api

---
+++ client/src/api/sessionStorageService.js
@@ -0,0 +1,17 @@
+export function saveUserData(userData) {
+    sessionStorage.setItem('auth', userData.authToken)
+    sessionStorage.setItem('email', userData.email)
+    sessionStorage.setItem('username', userData.username)
+    sessionStorage.setItem('userId', userData.userId)
+}
+
+export function removeUserData() {
+    sessionStorage.removeItem('auth')
+    sessionStorage.removeItem('email')
+    sessionStorage.removeItem('username')
+    sessionStorage.removeItem('userId')
+}
+
+export function getUsername() {
+    return sessionStorage.getItem('username')
+}
