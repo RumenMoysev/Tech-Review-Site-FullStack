8de8ed6bb2ef51187371bb929eb8e06513f0f018
RumenMoysev <rumoysev@gmail.com>
2023-11-06 17:58:42 +0200

    Import article and fetch data

--- client/src/components/Articles/Articles.jsx
+++ client/src/components/Articles/Articles.jsx
@@ -1,75 +1,31 @@
+import { useEffect, useState } from 'react';
 import './Articles.css'
+import Article from './ArticlesComponents/Article.jsx';
 
 export default function Articles() {
+    const [articles, setArticles] = useState(undefined)
+
+    useEffect(() => {
+        fetch('http://localhost:3030/data/reviews')
+        .then(x => x.json())
+        .then(data => {
+            if(data.length > 1) {
+                setArticles(data)
+            }
+        })
+        .catch(err => console.log(err))
+    }, [])
+
     return (
         <section id="articles" className="articles hidden">
-            <article className="hidden">
-                <h2>Should you buy the Galaxy S23 Ultra?</h2>
-                <div className="pImgContainer">
-                    <p>
-                        In this article we will look at some categories and see
-                        if the Galaxy S23 Ultra is worth buying.
-                    </p>
-                    <button>
-                        <a href="/S23Ultra">Details</a>
-                    </button>
-                </div>
-                <img
-                    src="https://images.samsung.com/is/image/samsung/p6pim/uk/feature/164370272/uk-feature-galaxy-s-535141640?$FB_TYPE_I_JPG$"
-                    alt="picture"
-                />
-            </article>
-            <article className="hidden">
-                <h2>Should you buy the Iphone 14 Pro Max?</h2>
-                <div className="pImgContainer">
-                    <p>
-                        In this article we will look at some categories and see
-                        if the Iphone 14 Pro Max is worth buying.
-                    </p>
-                    <button>
-                        <a href="/S23Ultra">Details</a>
-                    </button>
-                </div>
-                <img
-                    src="https://cdn11.bigcommerce.com/s-53cwszo4/images/stencil/2048x2048/products/1098/7283/14_pro_black_front__59165.1663003197.jpg?c=2"
-                    alt="picture"
-                />
-            </article>
-            <article className="hidden">
-                <h2>Iphone 14 Pro Max vs Galaxy S23 Ultra</h2>
-                <div className="pImgContainer">
-                    <p>
-                        In this article we will look at the main differences
-                        between the Iphone 14 Pro Max and the Galaxy S23 Ultra
-                        and see which one suits you best.
-                    </p>
-                    <button>
-                        <a href="/S23Ultra">Details</a>
-                    </button>
-                </div>
-                <img
-                    src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Samsung_Galaxy_S23_Ultra_vs_Apple_iPhone_14_Pro_Max_drdNBC.jpg"
-                    alt="picture"
-                />
-            </article>
-            <article className="hidden">
-                <h2>Iphone 14 Pro Max vs Galaxy S23 Ultra</h2>
-                <div className="pImgContainer">
-                    <p>
-                        In this article we will look at the main differences
-                        between the Iphone 14 Pro Max and the Galaxy S23 Ultra
-                        and see which one suits you best.
-                    </p>
-                    <button>
-                        <a href="/S23Ultra">Details</a>
-                    </button>
-                </div>
-                <img
-                    src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Samsung_Galaxy_S23_Ultra_vs_Apple_iPhone_14_Pro_Max_drdNBC.jpg"
-                    alt="picture"
-                />
-            </article>
-            <h2 className='hidden'>No reviews yet!</h2>
+            {articles
+            ?
+                articles.map(article => (
+                    <Article key={article._id} article={article}/>
+                ))
+            :
+                <h2 className='hidden'>No reviews yet!</h2>
+            }
         </section>
     );
 }
