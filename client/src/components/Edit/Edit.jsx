794506e673dea3551647879b14064806cca8f302
RumenMoysev <rumoysev@gmail.com>
2023-11-07 18:10:25 +0200

    Form is now controlled

--- client/src/components/Edit/Edit.jsx
+++ client/src/components/Edit/Edit.jsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+
 import './Edit.css'
 
+const initialEditState = {
+    title: '',
+    imageUrl: '',
+    summary: '',
+    details: ''
+}
 
 export default function Edit() {
+    const [editState, setEditState] = useState(initialEditState)
+    const [error, setError] = useState(undefined)
+
+    function editSubmitHandler(e) {
+        e.preventDefault()
+
+        const reviewData = editState
+        console.log(reviewData)
+    }
+
+    function editValueHandler(e) {
+        const name = e.target.name
+        const value = e.target.value
+
+        setEditState(state => ({
+            ...state,
+            [name]: value
+        })) 
+    }
+
     return (
         <section id="editPage" className="hidden editPage">
             <div className="form-container">
@@ -9,7 +37,7 @@ export default function Edit() {
                     <h2>Edit</h2>
                     {/* {error && <h4>{error}</h4>} */}
                 </div>
-                <form id="editForm" className="editForm">
+                <form id="editForm" className="editForm" onSubmit={editSubmitHandler}>
                     <label htmlFor="emailInput">Title</label>
                     <input
                         id="titleInput"
@@ -17,6 +45,8 @@ export default function Edit() {
                         placeholder="Example: S23 Ultra vs 15 Pro Max"
                         name="title"
                         type="text"
+                        value={editState.title}
+                        onChange={editValueHandler}
                         required
                     />
                     <label htmlFor="imageInput">Image URL</label>
@@ -25,6 +55,8 @@ export default function Edit() {
                         className="good imageInput"
                         placeholder="Example: https://someLink/image.jpg"
                         name="imageUrl"
+                        value={editState.imageUrl}
+                        onChange={editValueHandler}
                         required
                     />
                     <label htmlFor="summaryInput">Summary</label>
@@ -34,6 +66,8 @@ export default function Edit() {
                         placeholder="Example: 123abc"
                         name="summary"
                         type="text"
+                        value={editState.summary}
+                        onChange={editValueHandler}
                         required
                     />
                     <label htmlFor="detailsInput">Details</label>
@@ -43,6 +77,8 @@ export default function Edit() {
                         placeholder="Details"
                         name="details"
                         type="text"
+                        value={editState.details}
+                        onChange={editValueHandler}
                         required
                     />
                     <button type="submit">Confirm Edit</button>
