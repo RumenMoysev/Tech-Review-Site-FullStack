5d3b365ad3c633ca1c016ffb796dba940ecfb9d5
RumenMoysev <rumoysev@gmail.com>
2023-11-07 18:09:48 +0200

    Form is now controlled

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -5,13 +5,17 @@ import { saveUserData } from "../../api/sessionStorageService.js";
 import "./Login.css"
 
 export default function Login({setIsAuth}) {
-
+    const [emailValue, setEmailValue] = useState('')
+    const [passwordValue, setPasswordValue] = useState('')
     const [error, setError] = useState(undefined)
 
     async function loginHandler(e) {
         e.preventDefault()
 
-        const formData = new FormData(e.target.parentElement)
+        const formData = {
+            email: emailValue,
+            password: passwordValue
+        }
 
         const response = await loginService(formData)
         const json = await response.json()
@@ -26,6 +30,14 @@ export default function Login({setIsAuth}) {
         setIsAuth(true)
     }
 
+    function emailValueHandler(e) {
+        setEmailValue(e.target.value)
+    }
+
+    function passwordValueHandler(e) {
+        setPasswordValue(e.target.value)
+    }
+
     return (
         <section id="loginPage" className="hidden loginPage">
             <div className="form-container">
@@ -37,19 +49,23 @@ export default function Login({setIsAuth}) {
                     <label htmlFor="emailInput">Email</label>
                     <input
                         id="emailInput"
-                        className="good emailInput"
+                        className={`good emailInput ${error && 'warning'}`}
                         placeholder="Example: monkey@banana.com"
                         name="email"
                         type="email"
+                        value={emailValue}
+                        onChange={emailValueHandler}
                         required
                     />
                     <label htmlFor="passwordInput">Password</label>
                     <input
                         id="passwordInput"
-                        className="good passwordInput"
+                        className={`good passwordInput ${error && 'warning'}`}
                         placeholder="Example: 123abc"
                         name="password"
                         type="password"
+                        value={passwordValue}
+                        onChange={passwordValueHandler}
                         required
                     />
                     <div className="rememberMe-container">
