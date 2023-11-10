9c01cef66eb0cebad865018af89693b7dab03b36
RumenMoysev <rumoysev@gmail.com>
2023-11-10 20:08:48 +0200

    Moved validation to a function and add settings

--- client/src/api/reviewsService.js
+++ client/src/api/reviewsService.js
@@ -33,18 +33,11 @@ export function addReview(reviewData, setError) {
     return fetch(`${baseUrl}/data/reviews`, settings)
 }
 
-export function editReview(reviewData, setError) {
-    if (reviewData.title.length < titleLength) {
-        return setError(`Title should be at least ${titleLength} characters long`)
-    }
-    if (!reviewData.imageUrl.startsWith('http://') && !reviewData.imageUrl.startsWith('https://')) {
-        return setError('Please provie a valid image URL')
-    }
-    if (reviewData.summary.length < summaryLength) {
-        return setError(`Summary should be at least ${summaryLength} characters long`)
-    }
-    if (reviewData.description.length < descriptionLength) {
-        return setError(`Description should be at least ${descriptionLength} characters long`)
+export function editReview(reviewData, setError, reviewId) {
+    const error = validateData(reviewData)
+
+    if (error) {
+        return setError(error)
     } else {
         setError(undefined)
     }
@@ -52,19 +45,30 @@ export function editReview(reviewData, setError) {
     const authToken = getAuth()
 
     if (!authToken) {
-        return setError('You need to be logged in to add a review')
+        return setError('You need to be logged in to edit a review')
     } 
 
-    // const settings = {
-    //     method: "PUT",
-    //     headers: {
-    //         "Content-type": "application/json",
-    //         "X-Authorization": authToken
-    //     },
-    //     body: JSON.stringify(reviewData)
-    // }
+    const settings = {
+        method: "PUT",
+        headers: {
+            "Content-type": "application/json",
+            "X-Authorization": authToken
+        },
+        body: JSON.stringify(reviewData)
+    }
 
-    // return fetch(`${baseUrl}/data/reviews`, settings)
+    return fetch(`${baseUrl}/data/reviews/${reviewId}`, settings)
+}
+
+export function deleteReview(reviewId) {
+    const authToken = getAuth()
+
+    const settings = {
+        method: "DELETE",
+        headers: { "X-Authorization": authToken }
+    }
+
+    return fetch(`${baseUrl}/data/reviews/${reviewId}`, settings)
 }
 
 function validateData(reviewData) {
