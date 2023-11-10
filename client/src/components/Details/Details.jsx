6b7e53022900fd159be223a2f93cd41444547280
RumenMoysev <rumoysev@gmail.com>
2023-11-10 20:10:25 +0200

    Add details page and delete Handler

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -1,28 +1,48 @@
+import { useState, useEffect } from 'react';
+
+import { useParams, Link, useNavigate } from 'react-router-dom';
+
+import { deleteReview } from '../../api/reviewsService.js';
 import './Details.css'
 
 export default function Details() {
+    const [reviewDetails, setReviewDetails] = useState('')
+    const { reviewId } = useParams()
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        fetch(`http://localhost:3030/data/reviews/${reviewId}`)
+        .then(x => x.json())
+        .then(data => setReviewDetails(data))
+        .catch(err => console.log(err))
+    }, [])
+
+    async function deleteHandler() {
+        const response = await deleteReview(reviewId)
+
+        navigate('/reviews')
+    }
+
     return (
-        <section id="details" className="hidden">
+        <section id="details" className="hidden detailsPage">
             <div className="detailsContainer">
-                <h2>Should you buy the Galaxy S23 Ultra?</h2>
+                <h2>{reviewDetails.title}</h2>
                 <img
-                    src="https://images.samsung.com/is/image/samsung/p6pim/uk/feature/164370272/uk-feature-galaxy-s-535141640?$FB_TYPE_I_JPG$"
+                    src={reviewDetails.imageUrl}
                     alt="picture"
                 />
 
                 <div className="pBtnContainer">
-                    <p>
-                        In this article we will look at some categories and
-                        seeif the Galaxy S23 Ultra is worth buying.
-                    </p>
+                    <p>{reviewDetails.description}</p>
 
                     <div className="detailsBtns">
-                        <button>
-                            <a href="/S23Ultra">Edit</a>
-                        </button>
-                        <button>
-                            <a href="/ada">Delete</a>
-                        </button>
+                        <Link to="edit"><button>Edit</button></Link>
+                        
+                        {/* <button>
+                            <Link to="edit">Edit</Link>
+                        </button> */}
+                        <button onClick={deleteHandler}>Delete</button>
                         <button>
                             <a href="/dada">Like</a>
                         </button>
