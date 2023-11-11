6b43638b2bfef07e312649df1d001a089bc40343
RumenMoysev <rumoysev@gmail.com>
2023-11-11 09:26:28 +0200

    Add spinner while loading data

--- client/src/components/Reviews/Reviews.jsx
+++ client/src/components/Reviews/Reviews.jsx
@@ -1,31 +1,42 @@
 import { useEffect, useState } from 'react';
 import './Reviews.css'
 import Review from './ReviewsComponents/Review.jsx';
+import Spinner from '../Spinner/Spinner.jsx';
 
 export default function Articles() {
     const [articlesData, setArticlesData] = useState(undefined)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetch('http://localhost:3030/data/reviews')
-        .then(x => x.json())
-        .then(data => {
-            if(data.length > 0) {
-                setArticlesData(data)
-            }
-        })
-        .catch(err => console.log(err))
+            .then(x => x.json())
+            .then(data => {
+                setIsLoading(false)
+                if (data.length > 0) {
+                    setArticlesData(data)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                setIsLoading(false)
+            })
     }, [])
 
     return (
+        isLoading
+        ?
+        <Spinner />
+        :
         <section id="articles" className="articles ">
-            {articlesData
-            ?
-                articlesData.map(article => (
-                    <Review key={article._id} article={article}/>
-                ))
-            :
-                <h2 className='hidden'>No reviews yet!</h2>
+            {
+                articlesData
+                    ?
+                    articlesData.map(article => (
+                        <Review key={article._id} article={article} />
+                    ))
+                    :
+                    <h2 className='hidden'>No reviews yet!</h2>
             }
-        </section>
+        </section >
     );
 }
