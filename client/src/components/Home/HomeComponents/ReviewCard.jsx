9cc6ef7f744edec108ea537b3b864e0ce9066f0f
RumenMoysev <rumoysev@gmail.com>
2023-11-14 19:11:31 +0200

    Review card redirects to its review

--- client/src/components/Home/HomeComponents/ReviewCard.jsx
+++ client/src/components/Home/HomeComponents/ReviewCard.jsx
@@ -1,12 +1,22 @@
-export default function ReviewCard(data) {
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function ReviewCard({reviewData}) {
+    const [reviewId, setReviewId] = useState(undefined)
+    const navigate = useNavigate()
+
+    useEffect(() => setReviewId(reviewData._id), [])
+
+    const clickHandler = () => setTimeout(() => navigate(`/reviews/${reviewId}`), 60)
+
     return (
-        <div
+        <div onClick={clickHandler}
             className="card "
             style={{
-                backgroundImage: `url(${data.reviewData.imageUrl})`
+                backgroundImage: `url(${reviewData.imageUrl})`
             }}>
-            <h4>{data.reviewData.title}</h4>
-            <p>{data.reviewData.summary}</p>
+            <h4>{reviewData.title}</h4>
+            <p>{reviewData.summary}</p>
         </div>
     )
 }
