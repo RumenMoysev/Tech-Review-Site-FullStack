17ff585962328973f23179a52ca9d3350c3aba41
RumenMoysev <rumoysev@gmail.com>
2023-11-16 23:07:43 +0200

    Add likes on cards

--- client/src/components/Home/HomeComponents/ReviewCard.jsx
+++ client/src/components/Home/HomeComponents/ReviewCard.jsx
@@ -10,13 +10,13 @@ export default function ReviewCard({reviewData}) {
     const clickHandler = () => setTimeout(() => navigate(`/reviews/${reviewId}`), 60)
 
     return (
-        <div onClick={clickHandler}
-            className="card "
-            style={{
-                backgroundImage: `url(${reviewData.imageUrl})`
-            }}>
+        <div onClick={clickHandler} className="card " style={{ backgroundImage: `url(${reviewData.imageUrl})` }}>
             <h4>{reviewData.title}</h4>
             <p>{reviewData.summary}</p>
+            <div className="likeContainer">
+                <img src="/images/like.svg" />
+                <p className="likesCount">{reviewData.likes.length}</p>
+            </div>
         </div>
     )
 }
