6041db7af134da0e6df5e5b8cb0ea3514cc23937
RumenMoysev <rumoysev@gmail.com>
2023-11-20 11:18:08 +0200

    Show comment input only if logged in

--- client/src/components/Details/DetailsComponents/CommentSection.jsx
+++ client/src/components/Details/DetailsComponents/CommentSection.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
+import { AuthContext } from "../../../contexts/AuthContext.js"
 
-import { Link } from "react-router-dom"
+import Comment from "./Comment.jsx"
 
 export default function CommentSection() {
     const [commentFormData, setCommentFormData] = useState('')
+    const { isAuth } = useContext(AuthContext)
 
     const commentValueHandler = (e) => {
         setCommentFormData(e.target.value)
@@ -11,27 +13,18 @@ export default function CommentSection() {
 
     return (
         <div className="commentContainer">
-            <h3>Comments</h3>
-            <div className='commentInputBox'>
-                <input type='text' required="required" value={commentFormData} onChange={commentValueHandler}></input>
-                <span>Comment</span>
-                <button>Post</button>
-            </div>
+            <h3>Comments </h3>
+            {isAuth && 
+                <div className='commentInputBox'>
+                    <input type='text' required="required" value={commentFormData} onChange={commentValueHandler}></input>
+                    <span>Comment</span>
+                    <button>Post</button>
+                </div>
+            }
+            
             <div className='comments'>
-                <div className='comment'>
-                    <div className='commentData'>
-                        <Link to="/userId/profile">Username:</Link>
-                        <p>Some comment thast about this long</p>
-                    </div>
-                    <img src='/images/like.svg' />
-                </div>
-                <div className='comment'>
-                    <div className='commentData'>
-                        <Link to="/userId/profile">Username:</Link>
-                        <p>Some comment thast about this az sum niggaaaaaaaaaaaaaaa</p>
-                    </div>
-                    <img src='/images/like.svg' />
-                </div>
+                <Comment />
+                <Comment />
             </div>
         </div>
     )
