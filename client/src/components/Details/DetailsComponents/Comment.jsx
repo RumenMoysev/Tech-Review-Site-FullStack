aee7fe83f76c9a4c73957d8b5e66018128ea85a4
RumenMoysev <rumoysev@gmail.com>
2023-11-22 23:07:02 +0200

    Populate comment with data and add delete

--- client/src/components/Details/DetailsComponents/Comment.jsx
+++ client/src/components/Details/DetailsComponents/Comment.jsx
@@ -1,23 +1,31 @@
 import { useContext } from "react"
 import { Link } from "react-router-dom"
 import { AuthContext } from "../../../contexts/AuthContext.js"
+import { deleteComment } from "../../../api/reviewsService.js"
 
-export default function Comment() {
-    const {isAuth} = useContext(AuthContext)
+export default function Comment({ commentData, setComments, reviewId }) {
+    const { isAuth } = useContext(AuthContext)
 
     const onLikeClick = () => {
         console.log('Like clicked')
     }
 
+    const onDeleteClick = () => {
+        setComments(state => state.filter(comment => comment._id !== commentData._id))
+        deleteComment(reviewId, commentData._id)
+    }
+
     return (
     <div className='comment'>
         <div className='commentData'>
-            <Link to="/userId/profile">Username:</Link>
-            <p>Some comment thast about this long</p>
+            <Link to="/userId/profile">{commentData.owner.username}:</Link>
+            <p>{commentData.comment}</p>
         </div>
         <div className="likeData">
-            <p>2 likes</p>
-            {isAuth && <img src='/images/like.svg' onClick={onLikeClick} />}
+            {commentData.likes.length > 0 && <p>{commentData.likes.length} likes</p>}
+            {isAuth &&
+                commentData.isOwner ? <img src="/images/bin.svg" style={{ width: '19.5px' }} onClick={onDeleteClick} /> : <img src='/images/like.png' style={{ width: '19px' }} onClick={onLikeClick} />
+            }
         </div>
     </div>
     )
