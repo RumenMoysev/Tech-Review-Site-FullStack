ed29ef54bfb2f9358c199c9873e4c5277c4bda89
RumenMoysev <rumoysev@gmail.com>
2023-11-26 00:42:39 +0200

    Search bar works with enter too

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -5,9 +5,15 @@ import './SearchBar.css'
 export default function SearchBar({searchHandler}) {
     const [searchValue, setSearchValue] = useState('')
 
+    const onEnterDown = (e) => {
+        if(e.key === 'Enter') {
+            searchHandler(searchValue)
+        }
+    } 
+
     return(
         <div className='searchBar'>
-            <input type='text' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder='Search by title'></input>
+            <input type='text' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} onKeyDown={onEnterDown} placeholder='Search by title'></input>
             <img src='/images/loupe.svg' onClick={() => searchHandler(searchValue)}></img>
         </div>
     )
