8e47500371557f1fdfd4912c31768da677c3bb38
RumenMoysev <rumoysev@gmail.com>
2023-12-09 10:40:48 +0200

    Remove useEffect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,26 +19,6 @@ import Footer from "./components/Footer/Footer.jsx"
 import './App.css'
 
 function App() {
-    useEffect(() => {
-        const options = {
-            rootMargin: "-35% 0px -8% 0px",
-            // threshold: 0.3
-        };
-
-        const observer = new IntersectionObserver((e) => {
-            e.forEach((el) => {
-                if (el.isIntersecting) {
-                    el.target.classList.add("show");
-                } else {
-                    el.target.classList.remove('show');
-                }
-            });
-        }, options);
-
-        const hiddenElems = document.querySelectorAll(".hidden");
-        hiddenElems.forEach((el) => observer.observe(el));
-    })
-
     return (
         <AuthProvider>
             <Header/>
